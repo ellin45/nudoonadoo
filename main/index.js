2a5e2c9cc83ad19125c30b7991ff85eae44b90c3
정채림 <chaelimj45@gmail.com>
2023-08-05 22:25:28 +0900

    "한일 목록에서 바로 클릭 이벤트로 인풋창으로 넣는 기능 구현"

--- main/index.js
+++ main/index.js
@@ -8,12 +8,32 @@ $(document).ready(function() {
         if (name === '' || todo === '') {
             alert('이름과 한일을 모두 입력하세요.');
         } else {
-            $('#comment_list').append(`<li>이름: ${name}, 한일: ${todo}</li>`);
-            $('#name_input').val('');
-            $('#todo_input').val('');
+            // 새로운 리스트 항목을 생성하고 data 속성을 추가합니다.
+            var listItem = $("<li></li>")
+                .text(name + ": " + todo)
+                .attr("data-name", name)
+                .attr("data-todo", todo);
+
+            // 리스트에 새로운 항목을 추가합니다.
+            $("#comment_list").append(listItem);
+
+            // 입력창을 비웁니다.
+            $("#name_input").val("");
+            $("#todo_input").val("");
         }
     });
 });
+
+$(document).on("click", "#comment_list li", function() {
+    // 클릭한 항목의 데이터를 가져옵니다.
+    var name = $(this).attr("data-name");
+    var todo = $(this).attr("data-todo");
+
+    // 가져온 데이터를 칭찬 입력 창에 채워넣습니다.
+    $("#name_input_praise").val(name);
+    $("#praise_input").val("I appreciate you for " + todo);
+});
+
 
 $(document).ready(function() {
     $('#submit_button_praise').on('click', function(e) {
@@ -25,17 +45,25 @@ $(document).ready(function() {
         if (name_praised === '' || praise === '') {
             alert('이름과 칭찬 내용을 모두 입력하세요.');
         } else {
-            $('.praised_list').append(`<li>이름: ${name_praised}, 칭찬: ${praise}</li>`);
             if (name_praised === '엘린') {
-                $('#my_praise_list').append(`<li>내가 받은 칭찬: ${praise}</li>`);
+                $('.my_praised_list').append(`<li>이름: ${name_praised}, 칭찬: ${praise}</li>`);
             }
+
+            $('.praised_list').append(`<li>이름: ${name_praised}, 칭찬: ${praise}</li>`);
             $('#name_input_praise').val('');
             $('#praise_input').val('');
         }
     });
 });
 
-//나중에 한일목록을 클릭하면 한일 인풋창을 만들어서 자동입력되게 해두고 그 한일을 칭찬 바로 할수 있게 만들기
 
-
-
+$(document).on("click", "#comment_list li", function() {
+    // 클릭한 항목의 데이터를 가져옵니다.
+    let name = $(this).attr("data-name");
+    let todo = $(this).attr("data-todo");
+  
+    // 가져온 데이터를 칭찬 입력 창에 채워넣습니다.
+    $("#name_input_praise").val(name);
+    $("#praise_input").val(todo);
+  });
+  
